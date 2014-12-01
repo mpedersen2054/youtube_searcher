5f3b09514f742c35975434addc9403d73a2c0ead
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-01 12:13:53 -0600

    add a function in raw_data.js to put the iterated over important response data into an html template when user searches

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(function() {
     
     $('#search-results').on('submit', function(event) {
-        var term = $('#query').val();
-        url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyDAVluCs90lmYwADQjoQe3T3FB8IlhOux0&q='+term;
+        var term = $('#query').val(),
+            key = 'AIzaSyDAVluCs90lmYwADQjoQe3T3FB8IlhOux0',
+            url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key=+'+key+'+&q='+term;
         event.preventDefault();
 
         $.getJSON(url, function(data) {
@@ -13,14 +14,15 @@ $(function() {
     function showResults(results) {
 
         for (var i = 0; i < results.length; i++) {
-            var resObj = results[i].snippet,
-                resImg = resObj.thumbnails.medium.url,
-                resTitle = resObj.title,
-                resDesc = resObj.description;
-            // $('#results').append('<img src='+results[i].snippet.thumbnails.medium.url+'>');
-            console.log(resImg);
-            console.log(resTitle);
-            console.log(resDesc);
+            var resImg = results[i].snippet.thumbnails.medium.url,
+                resTitle = results[i].snippet.title,
+                resDesc = results[i].snippet.description,
+                html = '';
+
+            template = outputResultHtml(resImg, resTitle, resDesc)
+
+            $('#results').append(template)
+
         };
     }
 
--- js/raw_data.js
+++ js/raw_data.js
@@ -1,4 +1,17 @@
-var resultHTML;
-resultHTML+='<article class="result">';
-resultHTML+='<div class="thumbnail pull-left">';
-resultHTML+='<img src='+results[i].snippet.thumbnails.medium.url+'alt="">'
+// a=resImg,b=resTitle,c=resDesc
+function outputResultHtml(a,b,c) {
+    var resultHTML = '';
+    resultHTML+='<article class="result">'
+    resultHTML+="<div class='container'>"
+    resultHTML+='<div class="thumbnail pull-left">';
+    resultHTML+="<img src='"+a+"'>"
+    resultHTML+="</div>"
+    resultHTML+="<div class='meta'>"
+    resultHTML+="<h3>"+b+"</h3>"
+    resultHTML+="<p>"+c+"</p>"
+    resultHTML+="</div>"
+    resultHTML+="</div>"
+    resultHTML+="</article>"
+
+    return resultHTML;
+}
