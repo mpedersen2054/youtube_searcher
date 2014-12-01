ab683cd8c5649843765cc858c91c26a544360147
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-01 10:40:00 -0600

    add a little js, figure out how to send requests to api, next is back to finishing frontend

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 $(function() {
     
-    $('#search-results').on('submit', function() {
-        alert('helloz')
+    $('#search-results').on('submit', function(event) {
+        var term = $('#query').val();
+        url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyDAVluCs90lmYwADQjoQe3T3FB8IlhOux0&q='+term;
+        event.preventDefault();
+
+        $.getJSON(url, function(data) {
+            showResults(data.items)
+        })
     })
+
+    function showResults(results) {
+        for (var i = 0; i < results.length; i++) {
+            // console.log(results[i].snippet.title);
+            $('#results').append('<img src='+results[i].snippet.thumbnails.medium.url+'>');
+            // console.log(results[i].snippet.thumbnails.medium.url)
+        };
+    }
 
 })();
